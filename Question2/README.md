606c44947a2b75858efc908a26d833ed6ee64aaf
Ibaqu <aquib.zt@gmail.com>
2023-01-06 21:51:10 -0500

    Add Question 2

---
+++ Question2/README.md
@@ -0,0 +1,5 @@
+# Apple and Orange
+
+Find how many apples and oranges fall on the house
+
+https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true
---
+++ Question2/src/Solution.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'countApplesAndOranges' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER s    - start of house
+     *  2. INTEGER t    - end of house
+     *  3. INTEGER a    - apple tree
+     *  4. INTEGER b    - orange tree
+     *  5. INTEGER_ARRAY apples
+     *  6. INTEGER_ARRAY oranges
+     */
+
+    public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
+
+        int appleTally = 0;
+        int orangeTally = 0;
+
+        // Iterate thru apples first
+
+        for (int coord : apples) {
+            if (isOnHouse(a + coord, s, t)) {
+                appleTally++;
+            }
+        }
+
+        System.out.println(appleTally);
+
+        for(int coord : oranges) {
+            if (isOnHouse(b + coord, s, t)) {
+                orangeTally++;
+            }
+        }
+
+        System.out.println(orangeTally);
+
+    }
+
+    public static Boolean isOnHouse(int coord, int startHouse, int endHouse) {
+        return ((startHouse <= coord) && (coord <= endHouse));
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int s = Integer.parseInt(firstMultipleInput[0]);
+
+        int t = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int a = Integer.parseInt(secondMultipleInput[0]);
+
+        int b = Integer.parseInt(secondMultipleInput[1]);
+
+        String[] thirdMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int m = Integer.parseInt(thirdMultipleInput[0]);
+
+        int n = Integer.parseInt(thirdMultipleInput[1]);
+
+        List<Integer> apples = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> oranges = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.countApplesAndOranges(s, t, a, b, apples, oranges);
+
+        bufferedReader.close();
+    }
+}
